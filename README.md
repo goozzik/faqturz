9a7618e282c89d4984fbe99bd7fe61c8ec73f708
Jakub Gorzelak <jakub.gorzelak@gmail.com>
2017-12-10 12:28:51 +0100

    Add test coverage (#2)

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Faqturz
 
+[![Build Status](https://travis-ci.org/goozzik/faqturz.svg?branch=master)](https://travis-ci.org/goozzik/faqturz)
+[![Coverage Status](https://coveralls.io/repos/github/goozzik/faqturz/badge.svg)](https://coveralls.io/github/goozzik/faqturz)
+
 **TODO: Add description**
 
 ## Installation
@@ -18,4 +21,3 @@ end
 Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at [https://hexdocs.pm/faqturz](https://hexdocs.pm/faqturz).
-
--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Faqturz.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test]
     ]
   end
 
@@ -21,7 +23,8 @@ defmodule Faqturz.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:credo, "~> 0.8", only: [:dev, :test], runtime: false}
+      {:credo, "~> 0.8", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.7", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
